85de1135c0031e7b3bb898a40a075d8928db3677
muzaffarahmed1202 <muzaffarahmed1202@gmail.com>
2025-06-14 17:20:08 +0530

    fixed pipeline issue

--- src/itemDetails.js
+++ src/itemDetails.js
@@ -3,7 +3,7 @@ import React,{useContext} from "react";
 import UseCardContext from "./cardContextApi";
 
 const ItemDetails = ()=>{
-        const data = useContext(UseCardContext);
+        //const data = useContext(UseCardContext);
     return (<>
     <div className="card">
     Item Details
